375518125c5b4c92af316ff98e03dcf557e5235b
Yazouv <yazouv.pro@gmail.com>
2023-10-05 18:56:06 +0200

    add api + ajout version et visiteurs

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const fs = require('fs');
+document.addEventListener('DOMContentLoaded', function () {
+    var xhr = new XMLHttpRequest();
+    var xhr_post_only = new XMLHttpRequest();
 
-function ActuStats() {
-    fetch("/data/stats.json")
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            responseObject = JSON.parse(xhr.responseText);
 
-            fs.readFile("/data/stats.json", "utf8", function (err, data) {
-                if (err) throw err;
-                console.log(data);
-            });
-            document.getElementById("visiteurs").innerHTML = data.visiteurs_total;
-        });
-}
+            document.getElementById('visiteurs').innerHTML = responseObject.visiteurs_total;
+            document.getElementById('version').innerHTML = responseObject.version;
+            var visiteurs = parseInt(document.getElementById('visiteurs').innerHTML) + 1
+            xhr_post_only.open('GET', 'http://localhost:8080/setstats/visiteurs_total/' + visiteurs, true);
+            xhr_post_only.send(null);
+        }
+    }
 
-ActuStats();
+    xhr.open('GET', 'http://localhost:8080/getstats', true);
+    xhr.send(null);
+});
