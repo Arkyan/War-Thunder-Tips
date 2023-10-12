bb76d4f98b5699e429a6b3c61f41931da4c524c9
Yazouv <yazouv.pro@gmail.com>
2023-10-12 15:53:00 +0200

    update

--- app.js
+++ app.js
@@ -9,11 +9,28 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('visiteurs').innerHTML = responseObject.visiteurs_total;
             document.getElementById('version').innerHTML = responseObject.version;
             var visiteurs = parseInt(document.getElementById('visiteurs').innerHTML) + 1
-            xhr_post_only.open('GET', 'http://localhost:8080/setstats/visiteurs_total/' + visiteurs, true);
+            xhr_post_only.open('GET', 'https://watch2earn.zehosting.fr/getstats' + visiteurs, true);
+
+            xhr_post_only.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
             xhr_post_only.send(null);
         }
     }
 
-    xhr.open('GET', 'http://localhost:8080/getstats', true);
+    xhr.open('GET', 'https://watch2earn.zehosting.fr/getstats', true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send(null);
 });
+
+var xhr2 = new XMLHttpRequest();
+
+xhr2.open('GET', 'https://cors-anywhere.herokuapp.com/https://watch2earn.zehosting.fr/getstats', true);
+xhr2.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+xhr2.send(null);
+
+xhr2.onload = function () {
+    if (xhr2.status === 200) {
+        console.log(xhr2.responseText);
+    }
+}
