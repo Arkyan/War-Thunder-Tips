fffaa3be86539665dcec6d419d7d7c1d80ddc5b1
Yazouv <yazouv.pro@gmail.com>
2023-10-13 13:52:11 +0200

    update api with password

--- HOSTAPI/app.js
+++ HOSTAPI/app.js
@@ -5,6 +5,31 @@ const cors = require('cors')
 
 app.use(cors())
 
+app.use((req, res, next) => {
+
+    // -----------------------------------------------------------------------
+    // authentication middleware
+
+    const auth = { login: 'login', password: 'password' } // change this
+
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+    // Verify login and password are set and correct
+    if (login && password && login === auth.login && password === auth.password) {
+        // Access granted...
+        return next()
+    }
+
+    // Access denied...
+    res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+    res.status(401).send('Authentication required.') // custom message
+
+    // -----------------------------------------------------------------------
+
+})
+
 app.get('/', (req, res) => {
     res.send('Bienvenue sur l\'API de gestion de fichiers !')
 })
